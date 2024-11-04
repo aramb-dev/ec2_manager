12b7c0497d4910c68db0225cf7523e2047f025ab
Abdur Rahman Bilal <aramb@aramservices.com>
2024-11-04 17:53:03 -0500

    main.py
    simple

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+
+from gui import EC2ManagerApp
+
+def main():
+    # Create and run the main application window
+    app = EC2ManagerApp()
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
